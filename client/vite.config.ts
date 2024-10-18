f3c6147442ccce44abc1f30ec38bc9d208bb8c9f
shivampandey777 <shivampandey99242@gmail.com>
2024-10-18 10:55:44 +0530

    build changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,8 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+// vite.config.js
+import { splitVendorChunkPlugin } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,14 +14,21 @@ export default defineConfig({
   build: {
     rollupOptions: {
         output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
+          manualChunks(id: string) {
+            if (id.indexOf('node_modules') !== -1) {
+              const basic = id.toString().split('node_modules/')[1];
+              const sub1 = basic.split('/')[0];
+              if (sub1 !== '.pnpm') {
+                return sub1.toString();
+              }
+              const name2 = basic.split('/')[1];
+              return name2.split('@')[name2[0] === '@' ? 1 : 0].toString();
                
             }
-        }
+        },
+      },
     },
-    chunkSizeWarningLimit: 1600,
-}
+    // chunkSizeWarningLimit: 1600,
+  }
 })
+
